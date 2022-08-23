7fc043c09953e6be22f8704943dff2bc03d6f41b
AnomanderRK <yaret.mo.kymo@gmail.com>
2022-08-22 22:43:01 -0500

    Add basic endpoints

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"hello": "world"}
+
+
+@app.get("/tweets/{tweet_id}")
+def get_tweet(tweet_id):
+    return {tweet_id: "Some value"}
+
+
+@app.post("/users/{user_id}/details?age=30&height=184")
+def get_user():
+    return {}
+
+
+if __name__ == "__main__":
+    pass
