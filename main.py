35d5e74c6c3da5630f9fcbed6ba3a6bcee87d8b2
AnomanderRK <yaret.mo.kymo@gmail.com>
2022-08-25 22:47:19 -0500

    Add docstring for contact

--- main.py
+++ main.py
@@ -58,19 +58,19 @@ class LoginOut(BaseModel):
     message: str = Field(default="Login successfully!")
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@app.get("/", status_code=status.HTTP_200_OK, tags=["Main"])
 def home():
     return {"hello": "world"}
 
 
 @app.post("/person/new", response_model=Person, response_model_exclude={"password"},
-          status_code=status.HTTP_201_CREATED)
+          status_code=status.HTTP_201_CREATED, tags=["Persons"])
 def create_person(person: Person = Body(...)):
     # request body. Body makes the parameter needed
     return person
 
 
-@app.get("/person/detail", status_code=status.HTTP_200_OK)
+@app.get("/person/detail", status_code=status.HTTP_200_OK, tags=["Persons"])
 def show_person(name: Optional[str] = Query(default=None,
                                             min_length=1,
                                             max_length=50,
@@ -84,7 +84,7 @@ def show_person(name: Optional[str] = Query(default=None,
     return {name: age}
 
 
-@app.get("/person/detail/{person_id}", status_code=status.HTTP_200_OK)
+@app.get("/person/detail/{person_id}", status_code=status.HTTP_200_OK, tags=["Persons"])
 def show_person(person_id: int = Path(...,
                                       gt=0,
                                       title="Person's ID",
@@ -98,7 +98,7 @@ def show_person(person_id: int = Path(...,
     return {person_id: "It exists"}
 
 
-@app.put("/person/{person_id}", status_code=status.HTTP_200_OK)
+@app.put("/person/{person_id}", status_code=status.HTTP_200_OK, tags=["Persons"])
 def update_person(person_id: int = Path(...,
                                         gt=0,
                                         title="Person's ID",
@@ -112,18 +112,35 @@ def update_person(person_id: int = Path(...,
     return {person_id: results}
 
 
-@app.post(path="/login", response_model=LoginOut, status_code=status.HTTP_200_OK)
+@app.post(path="/login", response_model=LoginOut, status_code=status.HTTP_200_OK, tags=["Login"])
 def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
 
 
-@app.post(path="/contact", status_code=status.HTTP_200_OK)
+@app.post(path="/contact", status_code=status.HTTP_200_OK, tags=["Contact"],
+          summary="Leave a message!")
 def contact(first_name: str = Form(..., max_length=20, min_length=1, example="Anomander"),
             last_name: str = Form(..., max_length=20, min_length=1, example="Rake"),
             email: EmailStr = Form(..., example="example@example.com"),
             message: str = Form(min_length=20, max_length=300, example="This is a message example ..."),
             user_agent: Optional[str] = Header(default=None),
             ads: Optional[str] = Cookie(default=None)):
+    """
+    Post information about any concern the user might have. Take user agent information and cookies, if available
+
+    Parameters
+    ----------
+    - first_name: Person's first name
+    - last_name: Person's last name
+    - email: Person's email
+    - message: Message the person wants to communicate. More than 20 chars
+    - user_agent: Header information, if available
+    - ads: Cookies, if available
+
+    Returns
+    -------
+    dictionary with all the provided information
+    """
     return {
         "first_name": first_name,
         "last_name": last_name,
@@ -134,7 +151,7 @@ def contact(first_name: str = Form(..., max_length=20, min_length=1, example="An
     }
 
 
-@app.post("/post-image")
+@app.post("/post-image", tags=["Files"])
 def post_image(image: UploadFile = File(...)):
     return {
         "filename": image.filename,
